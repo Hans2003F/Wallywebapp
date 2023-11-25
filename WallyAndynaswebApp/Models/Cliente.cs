8771bb0e309fa42c91e7e7adf717d017d307e310
Franz <franzhansquispegarcia@gmail.com>
2023-11-25 07:41:47 -0400

    Se Agrego Key y Required a clientes

--- WallyAndynaswebApp/Models/Cliente.cs
+++ WallyAndynaswebApp/Models/Cliente.cs
@@ -1,9 +1,14 @@
-﻿namespace WallyAndynaswebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WallyAndynaswebApp.Models
 {
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string? NombreCompleto { get; set; }
+        [Required]
         public int CI { get; set; }
         public int Celular { get; set; }
         public DateTime FechaReserva { get; set; }
